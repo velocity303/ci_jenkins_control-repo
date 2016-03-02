496d4824c27a7dcd3321d6c6bf888241027c012b
James E. Jones <james.jones@puppetlabs.com>
2016-03-01 18:25:49 -0600

    update puppetmaster conf

--- site/profile/manifests/puppetmaster.pp
+++ site/profile/manifests/puppetmaster.pp
@@ -15,24 +15,6 @@ class profile::puppetmaster {
     notify     => Service['pe-puppetserver'],
   }
 
-  ini_setting { 'puppet.conf hiera_config main section' :
-    ensure  => present,
-    path    => "${::settings::confdir}/puppet.conf",
-    section => 'main',
-    setting => 'hiera_config',
-    value   => $hiera_yaml,
-    notify  => Service['pe-puppetserver'],
-  }
-
-  ini_setting { 'puppet.conf hiera_config master section' :
-    ensure  => absent,
-    path    => "${::settings::confdir}/puppet.conf",
-    section => 'master',
-    setting => 'hiera_config',
-    value   => $hiera_yaml,
-    notify  => Service['pe-puppetserver'],
-  }
-
   #remove the default hiera.yaml from the code-staging directory
   #after the next code manager deployment it should be removed
   #from the live codedir
